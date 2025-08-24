a6131603d9e2f79c0af478b6571002431dff78b7
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-08-24 13:04:12 +0800

    Add essentials

---
+++ essentials/src/app/header/header.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+
+export class Header {
+  protected readonly title = 'Easy Task';
+}
