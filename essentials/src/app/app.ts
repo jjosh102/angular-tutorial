3fdbde58980961dc2226b65172a946428675e043
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-08-25 10:44:09 +0800

    signals

--- essentials/src/app/app.ts
+++ essentials/src/app/app.ts
@@ -2,12 +2,22 @@ import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { User } from './user/user';
+import { Tasks } from './tasks/tasks';
+
+
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('essentials');
+  users = DUMMY_USERS;
+  selectedUser = signal('');
+
+  onUserSelected(userId: string): void {
+    this.selectedUser.set(DUMMY_USERS.find(user => user.id === userId)?.name || 'No name selected');
+  }
 }
--- essentials/src/app/user/user.ts
+++ essentials/src/app/user/user.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, Input, output } from '@angular/core';
+
+// import { DUMMY_USERS } from '../dummy-users';
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -6,6 +9,42 @@ import { Component } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
 
+
+// export class User {
+//   selectedUser = signal(DUMMY_USERS[randomIndex]);
+//   imagePath =  computed(() => `../assets/users/${this.selectedUser().avatar}`);
+
+//   onSelectUser(): void {
+//     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+//     this.selectedUser.set(DUMMY_USERS[randomIndex]);
+//   }
+
+
+// }
+// export class User {
+//   @Input({ required: true }) avatar!: string;
+//   @Input({ required: true }) name!: string;
+
+
+//   get imagePath(): string {
+//     return `../assets/users/${this.avatar}`;
+//   }
+
+//   onSelectUser(): void {
+
+//   }
+// }
+
+export class User {
+  id = input.required<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  select = output<string>();
+  imagePath = computed(() => `../assets/users/${this.avatar()}`);
+
+  onSelectUser(): void {
+    this.select.emit(this.id());
+  }
 }
