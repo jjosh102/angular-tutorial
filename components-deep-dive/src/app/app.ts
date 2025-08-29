532b4275d3a3b3f140ac6258df5d994e0a621209
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-08-29 09:24:42 +0800

    re-usable component

--- components-deep-dive/src/app/app.ts
+++ components-deep-dive/src/app/app.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { ServerStatus } from "./dashboard/server-status/server-status";
 import { Traffic } from "./dashboard/traffic/traffic";
+import { DashboardItem } from "./dashboard/dashboard-item/dashboard-item";
+import { Tickets } from "./dashboard/tickets/tickets";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  imports: [Header, ServerStatus, Traffic],
+  imports: [Header, ServerStatus, Traffic, DashboardItem, Tickets],
 })
 export class App {
 
