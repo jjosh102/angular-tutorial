c32c2a6477297a2f48d953851fc62023cd3990e6
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-08-31 09:18:25 +0800

    template variable

--- components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
+++ components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Button } from '../../../shared/button/button';
 import { Control } from "../../../shared/control/control";
 
@@ -10,4 +10,12 @@ import { Control } from "../../../shared/control/control";
 })
 export class NewTicket {
 
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
+
+
+  onSubmit(title: string, description: string) {
+
+    this.form?.nativeElement.reset();
+  }
 }
